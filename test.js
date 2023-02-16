8d676dc2f8d621a73bbe73d79c0ac45e46a9163a
MhdShahil <shahil.m@accubits.com>
2023-02-16 20:18:41 +0530

    modified code for BIP39 and BIP44

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const Tx = require("ethereumjs-tx").Transaction;
-const Web3 = require("web3");
+//const Web3 = require("web3");
 const ethers = require("ethers");
 const bip39 = require("bip39");
 const Wallet = require("ethereumjs-wallet");
 const HDKey = require("hdkey");
 const accountAddress = process.env.ACCOUNT_ADDRESS;
 const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
-const web3 = new Web3(
-  new Web3.providers.HttpProvider(
-    "https://goerli.infura.io/v3/44ba5f98ceea4f71ba411ab8d814e2b6"
-  )
-);
+// const web3 = new Web3(
+//   new Web3.providers.HttpProvider(
+//     "https://goerli.infura.io/v3/44ba5f98ceea4f71ba411ab8d814e2b6"
+//   )
+// );
 
-web3.eth.accounts.wallet.add(ACCOUNT_PRIVATE_KEY);
+//web3.eth.accounts.wallet.add(ACCOUNT_PRIVATE_KEY);
 
 // Generate a random mnemonic phrase with 12 words
 const mnemonic = bip39.generateMnemonic(128);
@@ -30,8 +30,12 @@ const seed = HDKey.fromMasterSeed(bip39.mnemonicToSeedSync(mnemonic));
 const extendedKey = seed.derive(path);
 
 // Create two wallet instances from the derived extended keys
-const wallet1 = Wallet.fromPrivateKey(extendedKey.deriveChild(0).privateKey);
-const wallet2 = Wallet.fromPrivateKey(extendedKey.deriveChild(1).privateKey);
+const wallet1 = Wallet["default"].fromPrivateKey(
+  extendedKey.deriveChild(0).privateKey
+);
+const wallet2 = Wallet["default"].fromPrivateKey(
+  extendedKey.deriveChild(1).privateKey
+);
 
 // Log the addresses of the generated wallets
 console.log("Address 1:", wallet1.getAddressString());
